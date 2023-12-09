c671ca30a342be5f70d789e60e8517eaf5a1bf5d
GeorgievQA <153357854+GeorgievQA@users.noreply.github.com>
2023-12-09 11:56:38 +0200

    Add files via upload

---
+++ 02SumArray/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine() //"5 3 6 3 4"
+                .Split(" ") // ["5", "3", "6", "3", "4"]
+                .Select(int.Parse) // [5, 3, 6, 3, 4]
+                .ToArray();
+
+int sum = 0;
+for  (int index = 0; index < numbers.Length; index++)
+{
+    sum += numbers[index];
+}
+Console.WriteLine(sum);
